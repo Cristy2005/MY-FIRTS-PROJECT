e9198e93f5a00c26becdd52cc29c4e6fd90eb2aa
Maria Cristina <mariacristinan147@gmail.com>
2025-05-07 22:00:04 -0300

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,19 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+
+
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+    importProvidersFrom(
+      MatToolbarModule,
+      MatIconModule,
+      MatSidenavModule
+    )
+  ]
 };
